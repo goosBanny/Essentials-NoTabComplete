3d5cc1b7cc799c276f0efd463d913bd8c72541ab
goosBanny <gooseissocool@gmail.com>
2025-08-07 11:20:49 +0500

    tabcomplete whitelist readme

--- Essentials/src/main/java/com/earth2me/essentials/Essentials.java
+++ Essentials/src/main/java/com/earth2me/essentials/Essentials.java
@@ -44,6 +44,7 @@ import com.earth2me.essentials.utils.AdventureUtil;
 import com.earth2me.essentials.utils.FormatUtil;
 import com.earth2me.essentials.utils.TaskUtil;
 import com.earth2me.essentials.utils.VersionUtil;
+import com.google.common.collect.Lists;
 import io.papermc.lib.PaperLib;
 import net.ess3.api.Economy;
 import net.ess3.api.IEssentials;
@@ -129,17 +130,7 @@ import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.MissingResourceException;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -619,17 +610,34 @@ public class Essentials extends JavaPlugin implements net.ess3.api.IEssentials {
         return commandMap;
     }
 
-//    @Override
-//    public List<String> onTabComplete(final @NotNull CommandSender sender, final @NotNull Command command, final @NotNull String commandLabel, final String[] args) {
-//        return onTabCompleteEssentials(sender, command, commandLabel, args, Essentials.class.getClassLoader(),
-//            "com.earth2me.essentials.commands.Command", "essentials.", null);
-//    }
+    @Override
+    public List<String> onTabComplete(final @NotNull CommandSender sender, final @NotNull Command command, final @NotNull String commandLabel, final String[] args) {
+        return onTabCompleteEssentials(sender, command, commandLabel, args, Essentials.class.getClassLoader(),
+            "com.earth2me.essentials.commands.Command", "essentials.", null);
+    }
 
+    protected List<String> getPlayers(final Server server, final User interactor) {
+        final List<String> players = Lists.newArrayList();
+        for (final User user : this.getOnlineUsers()) {
+            if (canInteractWith(interactor, user)) {
+                players.add(user.getName());
+            }
+        }
+        return players;
+    }
+
+    //WOOHOO HARDCODING!!!
+    String[] tabcompletable = {"warp", "sethome", "home", "renamehome", "delhome"};
     @Override
     public List<String> onTabCompleteEssentials(final CommandSender cSender, final Command command, final String commandLabel, final String[] args,
                                                 final ClassLoader classLoader, final String commandPath, final String permissionPrefix,
                                                 final IEssentialsModule module) {
-        if (!getSettings().isCommandOverridden(command.getName()) && (!commandLabel.startsWith("e") || commandLabel.equalsIgnoreCase(command.getName()))) {
+        User user = getUser((Player) cSender);
+
+        //early exit
+        if(Arrays.stream(tabcompletable).noneMatch(c -> c.equals(command.getName()))) return getPlayers(Bukkit.getServer(), user);
+
+        if ((!commandLabel.startsWith("e") || commandLabel.equalsIgnoreCase(command.getName()))) {
             final Command pc = alternativeCommandsHandler.getAlternative(commandLabel);
             if (pc instanceof PluginCommand) {
                 try {
@@ -644,12 +652,6 @@ public class Essentials extends JavaPlugin implements net.ess3.api.IEssentials {
         }
 
         try {
-            // Note: The tab completer is always a player, even when tab-completing in a command block
-            User user = null;
-            if (cSender instanceof Player) {
-                user = getUser((Player) cSender);
-            }
-
             final CommandSource sender = new CommandSource(this, cSender);
 
             // Check for disabled commands
